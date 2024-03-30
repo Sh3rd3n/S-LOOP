c9c92522e12aa66a8165c7f72f61c4e1deedc91c
Sh3rd3n <luca.derosas@eng.it>
2024-03-30 15:03:41 +0100

    Hero Section Set-Up

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		fontFamily: {
+			stencil: ['"Allerta Stencil"', ...defaultTheme.fontFamily.serif],
+			sans: ['"Alumni Sans"', ...defaultTheme.fontFamily.sans]
+		},
+		extend: {
+			backgroundImage: {
+				'hero-background': "url('/public/hero-base.jpg')"
+			},
+			colors: {
+				'teal': '#0BFCE1'
+			},
+		}
+	},
+	plugins: [],
+}
