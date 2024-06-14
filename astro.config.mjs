d916fdd7791ed32025f47f29c2bda6d4d25e05aa
Sh3rd3n <luca.derosas@hotmail.com>
2024-06-14 10:30:46 +0200

    New website url and fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://s-loop-sh3rd3ns-projects.vercel.app/',
   integrations: [tailwind()],
   // site: 'https://astro-photoswipe.petrovicz.com',
   i18n: {
