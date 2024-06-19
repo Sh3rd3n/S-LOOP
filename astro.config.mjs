a235c66598c064a4b5998b4b3c2c321a4df6eafb
Sh3rd3n <luca.derosas@hotmail.com>
2024-06-19 12:42:53 +0200

    New seo crawler metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://s-loop.it',
-  integrations: [tailwind()],
+  site: 'https://www.s-loop.it',
+  integrations: [tailwind(), sitemap()],
   // site: 'https://astro-photoswipe.petrovicz.com',
   i18n: {
     defaultLocale: "it",
     locales: ["it", "gb", "es", "fr", "de"],
-    prefixDefaultLocale: true,
+    prefixDefaultLocale: true
   }
 });
